c6e4245b2d1b0a32b7674b4fe84e7a634fff8d5f
Fatima <fcamero12@gmail.com>
2021-07-03 00:20:12 -0700

    Completed unicafe step 2

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -13,17 +13,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setScore(score + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setScore(score - 1)
+  }
+
+  let average
+  let positive
+  if (all === 0) {
+    average = 0
+    positive = 0
+  } else {
+    average = score / all
+    positive = (good / all) * 100
   }
 
   return (
@@ -36,6 +53,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>Total # of feedback: {all}</p>
+      <p>Average: { average }</p>
+      <p>Positive: { positive }%</p>
     </div>
   )
 }
